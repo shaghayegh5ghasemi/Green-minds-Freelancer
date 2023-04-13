48d5bb9eda8e2381441556061a462e92c2cbb18f
Qian Sun <69973222+Chin-Sun@users.noreply.github.com>
2023-04-12 21:27:08 -0400

    Update urls.py

--- jobs/urls.py
+++ jobs/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
-from .views import home, handle_login, profile, FreelancerCreateView, BusinessCreateView, FreelancerDetailView, BusinessDetailView
+from .views import home, view_business_profile, view_freelancer_profile, search, freelancer_report, business_report, FreelancerListViews, BusinessListViews
 
 urlpatterns = [
     path('', home),
-    path('freelancer/<int:pk>/', FreelancerDetailView.as_view(), name='freelancer-detail'),
-    path('business/<int:pk>/', BusinessDetailView.as_view(), name='business-detail'),
-    path('freelancer/create/', FreelancerCreateView.as_view(), name='freelancer-create'),
-    path('business/create/', BusinessCreateView.as_view(), name='business-create'),
-    path('account/setup', handle_login, name='handle-login'),
-    path('profile', profile, name='profile'),
+    path('freelancers/', FreelancerListViews.as_view(), name='freelancers-list'),
+    path('business/', BusinessListViews.as_view(), name='business-list'),
+    path('business/<int:pk>', view_business_profile, name='view-business-profile'),
+    path('freelancer/<int:pk>', view_freelancer_profile, name='view-freelancer-profile'),
+    path('search-results/', search, name='search-results'),
+    path('freelancer-report/<int:pk>', freelancer_report, name='freelancer-report'),
+    path('business-report/<int:pk>', business_report, name='business-report'),
 ]
